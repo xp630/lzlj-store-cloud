555196993032b1d9fdbbc5fb4a4101f748e45f67
xp630 <13442820@qq.com>
2026-05-27 18:06:36 +0800

    refactor(auth): 使用 com.lzlj.account.* 通配符简化 ComponentScan
    避免每次新增业务包都需要手动添加包名

--- account-biz-saas/account-biz-saas-auth/src/main/java/com/lzlj/account/user/SaasAuthApplication.java
+++ account-biz-saas/account-biz-saas-auth/src/main/java/com/lzlj/account/user/SaasAuthApplication.java
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.FilterType;
 @EnableFeignClients
 @MapperScan({"com.lzlj.account.user.dao", "com.lzlj.account.tenant.dao"})
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.lzlj.account.user", "com.lzlj.account.tenant", "com.lzlj.account.common"}, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = RedissonConfig.class))
+@ComponentScan(basePackages = {"com.lzlj.account.*"}, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = RedissonConfig.class))
 public class SaasAuthApplication {
 
     public static void main(String[] args) {
