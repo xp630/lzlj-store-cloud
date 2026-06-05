924acda2d85ed4b0cffa4e75c5a54b11c9f3e0dd
xp630 <13442820@qq.com>
2026-06-05 15:10:24 +0800

    fix: 优化组织机构查询

--- cloud-account-saas/cloud-account-saas-biz/cloud-account-saas-biz-auth/src/main/java/com/lzlj/account/biz/controller/SaasEnumController.java
+++ cloud-account-saas/cloud-account-saas-biz/cloud-account-saas-biz-auth/src/main/java/com/lzlj/account/biz/controller/SaasEnumController.java
@@ -6,11 +6,9 @@ import com.lzlj.account.common.core.enums.BusinessTypeEnum;
 import com.lzlj.account.common.core.enums.ParamTypeEnum;
 import com.lzlj.account.common.core.result.Result;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
